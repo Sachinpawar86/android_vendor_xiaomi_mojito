8122f1ddf476435b8beeaf58c00792629591e00e
Anush02198 <Anush.4376@gmail.com>
2021-11-23 20:30:01 +0530

    mojito:Add libcameraservice.so as prebuilt from stock
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- mojito-vendor.mk
+++ mojito-vendor.mk
@@ -6,6 +6,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/mojito
 
 PRODUCT_COPY_FILES += \
+    vendor/xiaomi/mojito/proprietary/system/system/lib/libcameraservice.so:$(TARGET_COPY_OUT_SYSTEM)/system/lib/libcameraservice.so \
     vendor/xiaomi/mojito/proprietary/vendor/bin/cnss-daemon:$(TARGET_COPY_OUT_VENDOR)/bin/cnss-daemon \
     vendor/xiaomi/mojito/proprietary/vendor/bin/hw/android.hardware.camera.provider@2.4-service_64:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.camera.provider@2.4-service_64 \
     vendor/xiaomi/mojito/proprietary/vendor/bin/hw/vendor.qti.hardware.sensorscalibrate@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.qti.hardware.sensorscalibrate@1.0-service \
